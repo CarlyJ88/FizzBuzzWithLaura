2eee81855c9375699014b6424153b7998c1b016e
Carly <carly.jenkinson65@googlemail.com>
2021-11-01 17:10:56 +0000

    Can pass a number.
    Co-authored-by:laura-enria <enria.laura2@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FizzBuzzWithLaura
+
+/* 
+   check if number is even and odd?
+   check if number is odd
+   check if number is even
+   what if we pass no argument?
+   what if we pass 0 as argument?
+*/
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,8 +1,23 @@
-/* 
-   check if number is even and odd?
-   check if number is odd
-   check if number is even
-   what if we pass no argument?
-   what if we pass 0 as argument?
-*/
+var fizzBuzz = require('./fizzbuzz');
 
+describe('FizzBuzz', function() {
+  it('can pass a number', function() {
+    expect(fizzBuzz(1)).toEqual(1);
+  })
+
+  // it('When passed a number that is a multiple of 3, the program returns the message "Fizz"', function() {
+
+  // })
+
+  // it('When passed a number that is a multiple of 5, the program returns the message "Buzz"', function() {
+
+  // })
+
+  // it('When passed a number that is a multiple of both 3 and 5, the program ignores the previous 2 rules and returns the message "FizzBuzz"', function() {
+
+  // })
+
+  // it('In all other cases, the program simply returns the given number.', function() {
+
+  // })
+})
