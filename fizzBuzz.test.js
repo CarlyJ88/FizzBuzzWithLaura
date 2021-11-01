c15260620dda4b419fbc7a314a285d09f196da0d
Carly <carly.jenkinson65@googlemail.com>
2021-11-01 17:27:03 +0000

    A multiple of 5 returns 'Buzz'.
    Co-authored-by:laura-enria <enria.laura2@gmail.com>

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -16,6 +16,12 @@ describe('FizzBuzz', function() {
     expect(fizzBuzz(5)).toEqual("Buzz");
   })
 
+
+  it('When passed another number that is a multiple of 5, the program returns the message "Buzz"', function() {
+    expect(fizzBuzz(20)).toEqual("Buzz");
+  })
+
+
   it('When passed a number that is a multiple of both 3 and 5, the program ignores the previous 2 rules and returns the message "FizzBuzz"', function() {
     expect(fizzBuzz(15)).toEqual("FizzBuzz");
   })
